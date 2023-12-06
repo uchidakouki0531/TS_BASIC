096d3fc581969e56196b803462883fb4af1f9a24
uchidakouki0531 <220087ww@yse-c.net>
2023-12-06 10:24:25 +0900

    hennkou

---
+++ user.ts
@@ -0,0 +1,22 @@
+// var user:{
+//     id:number;
+//     email:string;
+//     isActive:boolean;
+// }={
+//     id:1 ,
+//     email:"test@test.com",
+//     isActive:true,
+
+// }
+//user.id="32";
+var user={
+    id:1 ,
+    name:{
+        first:"Taro",
+        last:"Tokyo",
+    },
+    email:"test@test.com",
+    isActive:true,
+
+}
+console.log(user);
