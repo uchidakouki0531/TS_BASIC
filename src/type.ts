346438ee160ed4d02fb9f95bfb90a84c0ce42b2a
uchidakouki0531 <220087ww@yse-c.net>
2023-12-06 12:19:04 +0900

    hennkou

---
+++ src/type.ts
@@ -0,0 +1,26 @@
+//データ型に関するモジュール
+export type User = {
+    id: number;
+    name: {
+        first: string;
+        last: string;
+    },
+    email: string;
+    isActive: boolean;
+}
+export type Item = {
+    id: number;
+    name: string;
+    price: number;
+    isSale: boolean;
+}
+
+// export interface User {
+//     id: number;
+//     name: {
+//         first: string;
+//         last: string;
+//     },
+//     email: string;
+//     isActive: boolean;
+// }
