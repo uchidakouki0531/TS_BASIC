793509f0b79151a26896845d70cb02adc7622fad
uchidakouki0531 <220087ww@yse-c.net>
2023-12-06 10:25:41 +0900

    hennkou

--- user.ts
+++ user.ts
@@ -20,3 +20,23 @@ var user={
 
 }
 console.log(user);
+// 配列
+var itemNames:string[] = ["コーヒー", "紅茶", "ほうじ茶"];
+console.log(itemNames);
+
+//オブジェクト配列
+var item1 = {
+    id: 1,
+    name: "コーヒー",
+    price: 350,
+}
+var item2 = {
+    id: 2,
+    name: "紅茶",
+    price: 400,
+}
+var item3 = {
+    id: 3,
+    name: "ほうじ茶",
+    price: 300,
+}
